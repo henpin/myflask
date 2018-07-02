076695bdae5b635109728d22af0044d408d6d4e6
YumaKaminaga <kaminaga@nebit.co.jp>
2018-07-02 22:20:14 +0900

    Djashboardsファーストコミット
    * ファーストモック実装
        * ディスプレイイメージ
        * レイアウトイメージ
        * 機能イメージ

---
+++ djashboards/app.py
@@ -0,0 +1,75 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, Blueprint
+from flask import render_template, request, Response, jsonify, send_file, make_response, redirect, url_for, flash
+#from flask_tinydb import PDFFormDB, PDFFormCommitDataD
+
+import json, uuid
+import os, re
+from datetime import datetime
+import urllib
+
+from module_contextpath import get_contextRootPath
+from slack_interface import SlackInterface
+from chatwork_interface import ChatworkInterface
+from mail_utils import MailUtils
+
+# PATH
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MEDIA_DIR = os.path.join(BASE_DIR,"static","media") # /static/media
+URL_HEADER = "http://127.0.0.1:5000"
+
+# General Settings
+app = Flask(__name__)
+#app = Blueprint("djashboards",__name__,url_prefix="/djashboards",template_folder=get_contextRootPath(__file__,"templates"))
+app.secret_key = str(uuid.uuid4())
+
+# DB Setiings
+
+# Globals
+
+
+
+# Generals
+
+
+# Router
+@app.route("/")
+def home():
+    """ ワードファイルアップロード基底ペー ジ"""
+    # スラックタイムライン取得
+    slack = SlackInterface().inject_defaults()
+    slack_timeline = slack.get_message(5) # タイムライン抜く
+    slack_timeline.reverse() # 昇順にする
+
+    # スラックタイムライン２
+    slack.inject2("channel_id","CB52R5BLJ")
+    slack_timeline2 = slack.get_message(5) # タイムライン抜く
+    slack_timeline2.reverse() # 昇順にする
+
+    # チャットワークタイムライン取得
+    chatwork = ChatworkInterface().inject_defaults()
+    chatwork_timeline = chatwork.get_message(5)
+    chatwork_timeline.reverse() # 昇順にする
+
+    # メール取得
+    mail = MailUtils().inject_defaults()
+    mail_list = mail.get_mails() # 未読タイトル一覧取得
+
+    ns = {
+        "title" : u"HOME",
+    }
+
+    # 全フォームデータ抜く
+    return render_template("home.html",
+        slack_timeline = slack_timeline,
+        slack_timeline2 = slack_timeline2,
+        mail_list = mail_list,
+        chatwork_timeline = chatwork_timeline,
+        **ns
+        )
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
---
+++ djashboards/flask_tinydb.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from tinydb import TinyDB, Query
+import json
+import uuid
+from tinydb_utils import BaseTinyTable
+
+"""
+flask-tinyDBクライアント
+"""
+
+DB = TinyDB('flask.json')
+
+pass 
+
