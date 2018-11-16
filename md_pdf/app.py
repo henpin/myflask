73b93a2ff7e370988986acb61b91ef00ae9dc497
henpin <henpin@henpin.local>
2018-11-16 23:51:49 +0900

    dockerfileの追加

--- md_pdf/app.py
+++ md_pdf/app.py
@@ -5,15 +5,19 @@ import markdown2
 
 app = Flask(__name__)
 
-@app.route('/',methods=["POST"])
+@app.route('/',methods=["POST","GET"])
 def md2pdf():
     """ mdをPDFに """
-    # MD抜く
-    md = request.form['md']
-    # HTML化
-    html = markdown2.markdown(md,extras=["tables","fenc-code-blocks","code-color"])
-    # PDF化して返す
-    return render_pdf(HTML(string=html))
+    if request.method == "GET":
+        return "RUNNING"
+
+    elif request.method == "POST":
+        # MD抜く
+        md = request.form['md']
+        # HTML化
+        html = markdown2.markdown(md,extras=["tables","fenc-code-blocks","code-color"])
+        # PDF化して返す
+        return render_pdf(HTML(string=html))
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port=8000)
