2732ef89e6e77966d91f6cddcc78c6d1c704f0d0
henpin <henpin@henpin.local>
2018-12-15 01:12:20 +0900

    疎通したが日本語周りで断念

--- md_pdf/app.py
+++ md_pdf/app.py
@@ -9,28 +9,32 @@ app = Flask(__name__)
 
 # Enable Cross origin Access
 cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # HEROKU用
 port = os.environ.get("PORT") or "8000"
-
 
 @app.route('/',methods=["POST","GET"])
 def md2pdf():
     """ mdをPDFに """
     if request.method == "GET":
         md = "# MD PDF Program\n ## STATE: is runnning"
+        md = "# あらららららっらららららららあｒ"
+
         # HTML化
         html = markdown2.markdown(md,extras=["tables","fenc-code-blocks","code-color"])
         # PDF化して返す
-        return render_pdf(HTML(string=html))
+        return render_pdf(HTML(string=html),["app/static/general.css"])
 
     elif request.method == "POST":
         # MD抜く
-        md = request.form['md'] or "# No MD"
+        md = request.form.get('md') or "# No MD"
         # HTML化
         html = markdown2.markdown(md,extras=["tables","fenc-code-blocks","code-color"])
         # PDF化して返す
-        return render_pdf(HTML(string=html))
+        return render_pdf(HTML(string=html),"general.css")
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0',port=port)
+
